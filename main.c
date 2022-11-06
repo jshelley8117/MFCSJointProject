3d191fc26aca000345fc76b8d5c193abbd10aa96
jshelley17 <jshelley8117@gmail.com>
2022-11-06 10:27:17 -0600

    second change to repository to make sure updates are going through

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+Josh Shelley, Ria James, Zachary Sierra
+CS-3333-001
+Main file for Math Foundations Project
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Test to see if commits are going through on github.\n");
+}
